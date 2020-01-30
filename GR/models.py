fc374669f671e3473ce3e3725f14ba5ce157ff9a
tanishqdayma <tanishqdayma@gmail.com>
2020-01-30 10:59:39 +0530

    Change in Footer

--- GR/models.py
+++ GR/models.py
@@ -6,6 +6,7 @@ from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
 from streams import blocks
+from wagtail.search import index
 
 class GRPage(Page):
     """GR Page Class"""
@@ -17,14 +18,12 @@ class GRPage(Page):
     content = StreamField(
         [
             ("GR", blocks.ListAndButtonBlock()),
-            #("related", blocks.RelatedListAndButtonBlock()),
         ],
         null=True,
         blank=True,
         #label = "Main GR Docs",
     )
 
-   
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
         StreamFieldPanel("content"),
--- GR/views.py
+++ GR/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# class GRPage(RoutablePageMixin, Page):
+#     @route(r'^search/$')
+#     def post_search(self, request, *args, **kwargs):
+#         search_query = request.GET.get('q', None)
+#         self.posts = self.get_posts()
+#         if search_query:
+#             self.posts = self.posts.filter(body__contains=search_query)
+#             self.search_term = search_query
+#             self.search_type = 'search'
+#         return Page.serve(self, request, *args, **kwargs)
